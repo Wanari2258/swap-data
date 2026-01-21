13130f31c82e71bba969c383bbf2193b30bb8759
Wanari2258 <steptoroad.ks@gmail.com>
2026-01-21 03:57:50 +0100

    Update swap data 2026/01/21  3:57:50.01

--- push_swap.bat
+++ push_swap.bat
@@ -1,10 +1,13 @@
- @echo off
+@echo off
   cd /d C:\swap-data
 
   copy /Y "C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\EB299DF3DF8E2F9A1C0723943438596E\MQL5\Files\swap_fxgt.csv" "C:\swap-data\swap_fxgt.csv"
 
   copy /Y "C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\AE2CC2E013FDE1E3CDF010AA51C60400\MQL5\Files\swap_vantage.csv" "C:\swap-data\swap_vantage.csv"
 
+  copy /Y "C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\D0162EDAA10BE0147537DE4C04EABA64\MQL5\Files\swap_swift.csv" "C:\swap-data\swap_swift.csv"
+
   git add .
   git commit -m "Update swap data %date% %time%"
   git push origin main
+
